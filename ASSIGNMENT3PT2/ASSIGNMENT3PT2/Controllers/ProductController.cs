62666a5417b5e1a08146e876d1ad76648ae22f16
antthe176160 <anlalahello@gmail.com>
2023-07-11 23:21:38 +0700

    ass3 pt2

---
+++ ASSIGNMENT3PT2/ASSIGNMENT3PT2/Controllers/ProductController.cs
@@ -0,0 +1,96 @@
+﻿using ASSIGNMENT3PT2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASSIGNMENT3PT2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (MyDB3Context context = new MyDB3Context())
+            {
+                // Danh sach cate
+                ViewBag.pCode = context.Products.Select(p => p.ProductCode).ToList();
+
+                // List product
+                var products = context.Products.ToList();
+
+                return View(products);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Index(string productCode, string action, string newName, string oldPrice, string newPrice, string newImage, string newU)
+        {
+            using (MyDB3Context context = new MyDB3Context())
+            {
+                var products = context.Products.Where(p => p.ProductCode == productCode).ToList();
+                // Danh sach cate
+                ViewBag.pCode = context.Products.Select(p => p.ProductCode).ToList();
+
+                ViewBag.SelectedCode = productCode;
+
+                var product = context.Products.FirstOrDefault(p => p.ProductCode == productCode);
+
+                if (product != null)
+                {
+                    ViewBag.SelectedProduct = product;
+
+                    if (action == "update")
+                    {
+                        product.ProductName = newName;
+                        if (float.TryParse(newPrice, out float parsedPrice))
+                        {
+                            product.Price = parsedPrice;
+                        }
+                        product.Image = newImage;
+                        product.Unit = newU;
+
+                        context.SaveChanges();
+                    }
+
+                    
+
+                }
+
+                if (action == "filName")
+                {
+                    if (newName != null || !newName.Equals(""))
+                    {
+                        products = context.Products.ToList();
+                        products = products.Where(p => p.ProductName.Contains(newName)).ToList();
+                    if (products == null || products.Count ==0)
+                    {
+                        products = context.Products.ToList();
+                    }
+                    }
+                }else if(action == "filPrice")
+                {
+                    if (float.TryParse(newPrice, out float parsedNewPrice) && float.TryParse(oldPrice, out float parsedOldPrice))
+                    {
+                        products = context.Products.ToList();
+                        products = products.Where(p => p.Price >= parsedOldPrice && p.Price <= parsedNewPrice).ToList();
+                    }
+                    else
+                    {
+                        products = context.Products.ToList();
+                    }
+                }else if (action == "filImg")
+                {
+                    if (newImage != null || !newImage.Equals(""))
+                    {
+                        products = context.Products.ToList();
+                        products = products.Where(p => p.Image.Contains(newImage)).ToList();
+                        if (products == null || products.Count == 0)
+                        {
+                            products = context.Products.ToList();
+                        }
+                    }
+                }
+                
+                return View(products);
+            }
+        }
+
+    }
+}
---
+++ ASSIGNMENT3PT2/ASSIGNMENT3PT2/Models/MyDB3Context.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ASSIGNMENT3PT2.Models
+{
+    public partial class MyDB3Context : DbContext
+    {
+        public MyDB3Context()
+        {
+        }
+
+        public MyDB3Context(DbContextOptions<MyDB3Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                              .SetBasePath(Directory.GetCurrentDirectory())
+                              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyCnn"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasKey(e => e.CustomerCode);
+
+                entity.Property(e => e.CustomerCode)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Address).HasMaxLength(50);
+
+                entity.Property(e => e.CustomerName).HasMaxLength(50);
+
+                entity.Property(e => e.Dob)
+                    .HasColumnType("datetime")
+                    .HasColumnName("DOB");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(e => e.ProductCode);
+
+                entity.Property(e => e.ProductCode)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Image).HasMaxLength(50);
+
+                entity.Property(e => e.ProductName).HasMaxLength(50);
+
+                entity.Property(e => e.Unit)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
